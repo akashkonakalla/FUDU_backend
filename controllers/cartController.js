6e587026de959a22e6e03d2d3d045d992ea64815
Akash <konakalla.akash@gmail.com>
2025-12-07 01:25:27 +0530

    feat: add removeFromCart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -82,5 +82,26 @@ const updateQuantity = async (req, res) => {
   }
 };
 
+const removeFromCart = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { productId } = req.body;
 
+    let cart = await Cart.findOne({ user: userId });
+    if (!cart) return res.status(404).json({ error: "Cart not found" });
+
+    cart.items = cart.items.filter(
+      (i) => i.product.toString() !== productId
+    );
+
+    await cart.save();
+    res.status(200).json({ message: "Item removed", cart });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports= { addToCart, getCart, updateQuantity, removeFromCart};
 
